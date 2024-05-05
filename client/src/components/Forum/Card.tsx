755a8d5952663987cca6884397989b9db688b726
Artenida <artenidadaci@gmail.com>
2024-05-06 00:13:14 +0200

    display only a part of description on post and view more or less

--- client/src/components/Forum/Card.tsx
+++ client/src/components/Forum/Card.tsx
@@ -2,6 +2,7 @@ import Comments from "./Comments";
 import UserAccount from "./UserAccount";
 import userProfile from "../../assets/cover1.jpg";
 import ImageSlider from "../ImageSlider";
+import { useState } from "react";
 
 export interface BlogCardProps {
   posts: Paginated[] | null;
@@ -31,9 +32,16 @@ const Card: React.FC<Paginated> = ({
   images,
   user_id
 }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const shouldShowReadMore = description.length > 300;
+
+  const toggleDescription = () =>{ 
+    setIsExpanded(!isExpanded);
+  }
+
   return (
     <div className="md:pl-44 md:items-end mb-8 flex"> 
-      <div className="bg-white w-full md:w-[800px] h-auto rounded-lg p-4 md:flex-shrink-0 ">
+      <div className="bg-white w-full md:w-[680px] h-auto rounded-lg p-4 md:flex-shrink-0 ">
         <div className="flex flex-col">
           <div className="mb-4 md:mb-0 md:mr-4 md:flex-shrink-0">
             <UserAccount
@@ -45,13 +53,16 @@ const Card: React.FC<Paginated> = ({
           </div>
           <div className="flex flex-col mt-2"> 
             <h2 className="font-semibold">
-              {title.length > 30 ? title.substring(0, 30) + "..." : title}
+              {title}
             </h2>
             <p className="mt-2">
-              {description.length > 145
-                ? description.substring(0, 100) + "..."
-                : description}
+              {isExpanded ? description : (shouldShowReadMore ? description.substring(0, 300) + "..." : description)}
             </p>
+            {shouldShowReadMore && (
+              <span className="flex justify-end text-sm text-custom-color3 cursor-pointer" onClick={toggleDescription}>
+                {isExpanded ? "Read less..." : "Read more..."}
+              </span>
+            )}
           </div>
         </div>
         <div className="mt-4">
