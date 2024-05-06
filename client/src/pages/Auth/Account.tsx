3fbca5f5c03bf2c103e056fae79c27e79ae8b7d0
Artenida <artenidadaci@gmail.com>
2024-05-06 19:37:43 +0200

    made page responsive

--- client/src/pages/Auth/Account.tsx
+++ client/src/pages/Auth/Account.tsx
@@ -5,18 +5,18 @@ import Studies from "../../components/Account/Studies";
 
 const Account = () => {
   return (
-    <div className="my-20 mx-4 md:mx-28 grid grid-cols-1 md:grid-cols-2 gap-6">
-      <div className="flex gap-4 flex-col">
+    <div className="py-20 mx-4 md:mx-28 sm:ml-32 grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="flex gap-4 flex-col ml-16">
         <ProfilePicture />
         <div className="bg-custom-color1 h-[490px] rounded-xl p-4">
           <PersonalInfo/>
         </div>
       </div>
       <div className="flex justify-center gap-4 flex-col">
-        <div className="bg-custom-color1 h-[200px] rounded-xl p-4">
+        <div className="bg-custom-color1 h-[200px] rounded-xl p-4 ml-16">
         <Studies />
         </div>
-        <div className="bg-custom-color1 h-[400px] rounded-xl p-4">
+        <div className="bg-custom-color1 h-[400px] rounded-xl p-4 ml-16">
           <SocialAccounts />
         </div>
       </div>
